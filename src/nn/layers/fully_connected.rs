76863fbadebed5d8cc888251efba8f12b66b880d
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-17 22:06:45 +0000

    add fully connected layer

---
+++ src/nn/layers/fully_connected.rs
@@ -0,0 +1,17 @@
+use crate::nn::layers::layer::Layer;
+
+pub struct FullyConnected;
+impl Layer for FullyConnected {
+
+    fn new() -> Self {
+        FullyConnected {
+
+        }
+    }
+
+    fn forward(&self) {
+        todo!()
+    }
+}
+
+
