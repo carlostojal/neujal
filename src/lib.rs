08075ca19366e12ca0cd566ba7e670f729e20ac8
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-17 23:23:49 +0000

    write main library documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,27 @@
 
-pub mod nn;
+//!
+//! # Neujal
+//! 
+//! A neural network library written in Rust for learning purposes.
+//! 
+//! ## Features
+//! 
+//! - [ ] Feedforward
+//! - [ ] Backpropagation
+//! - [ ] Convolutional Neural Networks
+//! 
+//! 
+//! ## Examples
+//! 
+//! ```rust
+//! use neujal::nn::layers::fully_connected::FullyConnected;
+//! use neujal::nn::sequential::Sequential;
+//! 
+//! let mut model: Sequential = Sequential::new();
+//! 
+//! model.add(Box::new(FullyConnected::new()));
+//! model.add(Box::new(FullyConnected::new()));
+//! ```
+//! 
 
-// TODO: unit tests here
+pub mod nn;
