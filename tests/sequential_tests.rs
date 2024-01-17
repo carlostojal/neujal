8fd5dbceb8f211a9d186186a60b528a898b6001d
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-17 22:13:49 +0000

    implement first sequential model tests

---
+++ tests/sequential_tests.rs
@@ -0,0 +1,19 @@
+use std::sync::mpsc::TrySendError::Full;
+use neujal::nn::sequential::Sequential;
+use neujal::nn::layers::fully_connected::FullyConnected;
+
+#[test]
+fn create_sequential() {
+    let model: Sequential = Sequential::new();
+    assert_eq!(model.get_layers().len(), 0);
+}
+
+#[test]
+fn add_layers() {
+    let model: Sequential = Sequential::new();
+
+    model.add(Box::new(FullyConnected::new()));
+    model.add(Box::new(FullyConnected::new()));
+
+    assert_eq!(model.get_layers().len(), 2);
+}
