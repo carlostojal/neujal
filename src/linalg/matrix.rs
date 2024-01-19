6e834d24e3041a26ee4a47305e415d38a3518894
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-19 00:06:51 +0000

    overload matrix comparison operator. test addition

--- src/linalg/matrix.rs
+++ src/linalg/matrix.rs
@@ -2,6 +2,7 @@ use std::ops;
 use std::fmt;
 use std::fmt::{Formatter};
 use std::ops::{Index, IndexMut};
+use std::cmp;
 use rand::Rng;
 
 ///
@@ -143,6 +144,36 @@ impl ops::Add<Matrix> for Matrix {
     }
 }
 
+// overload comparison operator
+impl PartialEq<Self> for Matrix {
+    fn eq(&self, other: &Self) -> bool {
+
+        // for each row
+        for _x in 0..self.shape.0 {
+
+            // for each column
+            for _y in 0..self.shape.1 {
+                let mut diff: f32 = 0.0;
+                if self.values[_x][_y] > other.values[_x][_y] {
+                    diff = self.values[_x][_y] - other.values[_x][_y];
+                } else {
+                    diff = other.values[_x][_y] - self.values[_x][_y]
+                }
+
+                if diff >= f32::EPSILON {
+                    return false
+                }
+            }
+        }
+
+        true
+    }
+}
+
+impl Eq for Matrix {
+
+}
+
 // overload index operator
 impl Index<usize> for Matrix {
     type Output = Vec<f32>;
@@ -209,8 +240,69 @@ mod tests {
         assert_eq!(matrix.to_string(), s);
     }
 
-    // TODO: multiplication tests
+    #[test]
+    fn test_add() {
+        let mut m1: Matrix = Matrix::new((2, 2), false);
+        m1[0][0] = 1.0;
+        m1[0][1] = 2.0;
+        m1[1][0] = 3.0;
+        m1[1][1] = 4.0;
 
-    // TODO: addition tests
+        let mut m2: Matrix = Matrix::new((2, 2), false);
+        m2[0][0] = 4.0;
+        m2[0][1] = 3.0;
+        m2[1][0] = 2.0;
+        m2[1][1] = 1.0;
+
+        let mut expected: Matrix = Matrix::new((2, 2), false);
+        expected[0][0] = 5.0;
+        expected[0][1] = 5.0;
+        expected[1][0] = 5.0;
+        expected[1][1] = 5.0;
+
+        assert!(m1+m2 == expected);
+    }
+
+    #[test]
+    fn test_add_wrong() {
+        let mut m1: Matrix = Matrix::new((2, 2), false);
+        m1[0][0] = 1.0;
+        m1[0][1] = 2.0;
+        m1[1][0] = 3.0;
+        m1[1][1] = 4.0;
+
+        let mut m2: Matrix = Matrix::new((2, 2), false);
+        m2[0][0] = 4.0;
+        m2[0][1] = 5.0;
+        m2[1][0] = 2.0;
+        m2[1][1] = 1.0;
+
+        let mut not_expected: Matrix = Matrix::new((2, 2), false);
+        not_expected[0][0] = 5.0;
+        not_expected[0][1] = 5.0;
+        not_expected[1][0] = 5.0;
+        not_expected[1][1] = 5.0;
+
+        assert!(!(m1+m2 == not_expected));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_incompatible() {
+        let mut m1: Matrix = Matrix::new((1, 2), false);
+        m1[0][0] = 1.0;
+        m1[0][1] = 2.0;
+
+        let mut m2: Matrix = Matrix::new((2, 2), false);
+        m2[0][0] = 4.0;
+        m2[0][1] = 5.0;
+        m2[1][0] = 2.0;
+        m2[1][1] = 1.0;
+
+        let out: Matrix = m1 + m2;
+
+    }
+
+    // TODO: multiplication tests
 }
 
